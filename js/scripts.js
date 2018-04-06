a5ae0926420d96398dbc2875f0bb4036a7fd6563
Zach Spenard <Zacharyspenard@yahoo.com>
2018-04-06 16:46:50 -0700

    Add Js prototype for constructor

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,20 @@
 	$(document).ready(function() {
 $("form#pizzaOrder").submit(function(event) {
 event.preventDefault();
-
-
-
 // Back end logic
 // Constructor:
-function Pizza (size, cheese, veggie, meat) {
+function Pizza (size,cheese,veggies,meat) {
 	this.size = size;
 	this.cheese = cheese;
-	this.meat = veggie;
-	this.veggie = meat;
+	this.veggies = veggies;
+	this.meat = meat;
 }
+// Prototype:
+Pizza.prototype.cost = function() {
+	  var pizzaSizes = {"sm" : 15, "md" : 17.50, "lg" : 19.00, "xl" : 22.5};
+	  var pizzaCost = pizzaSizes [this["size"]];
+	  var cheeseCost = 1 * this["cheese"].length;
+		var veggiesCost = 1.50 * this["veggies"].length;
+	  var meatCost = 2 * this["meat"].length;
+	  return pizzaCost + cheeseCost + meatCost + veggiesCost;
+	}
