fcc419bb2d428aac7dcfa86a9fb5e0af1260b791
Zach Spenard <Zacharyspenard@yahoo.com>
2018-04-07 20:19:33 -0700

    It will give you the option to make multiple Pizza's by adding as many as you'd like and then checking out.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 // user interface logic
-	$(document).ready(function() {
-$("form#pizzaOrder").submit(function(event) {
-event.preventDefault();
-var size = $('input[name=size]:checked').val();
-var cheese = new Array();
-	 $('input[name=cheese]:checked').each(function() {
-		 cheese.push($(this).val());
-	 });
- 	var veggies = new Array();
-	 $('input[name=veggies]:checked').each(function() {
-		 veggies.push($(this).val());
-	 });
-	 var meat = new Array();
-	 $('input[name=meat]:checked').each(function() {
-		 meat.push($(this).val());
-	 });
-	 //call the pizza constructor:
-	 var order = new Pizza (size, cheese, veggies, meat);
-	 //get price for this order by calling the prototype cost function
-	 var orderCost = order.cost();
-	 $("#orderTotal").text("Thank you! Your order total is: $ " + orderCost);
- 	});
+$(document).ready(function() {
+	var pizzaArray = new Array();
+	$("#addPizza").click(function() {
+		var size = $('input[name=size]:checked').val();
+		var cheese = new Array();
+		$('input[name=cheese]:checked').each(function() {
+			cheese.push($(this).val());
+		});
+		var veggies = new Array();
+		$('input[name=veggies]:checked').each(function() {
+			veggies.push($(this).val());
+		});
+		var meat = new Array();
+		$('input[name=meat]:checked').each(function() {
+			meat.push($(this).val());
+		});
+		var order = new Pizza (size, cheese, veggies, meat);
+		pizzaArray.push(order);
+	});
+	$("form#pizzaOrder").submit(function(event) {
+		event.preventDefault();
+		var totalCost = 0;
+		for (var i = 0; i < pizzaArray.length; i++) {
+			totalCost += pizzaArray[i].cost();
+		}
+		$("#orderTotal").text("Thank you! Your order total is: $ " + totalCost);
+	});
 });
 // Back end logic
 // Constructor:
@@ -32,10 +37,10 @@ function Pizza (size,cheese,veggies,meat) {
 }
 // Prototype:
 Pizza.prototype.cost = function() {
-	  var pizzaSizes = {"sm" : 15, "md" : 17.50, "lg" : 19.00, "xl" : 22.5};
-	  var pizzaCost = pizzaSizes [this["size"]];
-	  var cheeseCost = 1 * this["cheese"].length;
-		var veggiesCost = 1.50 * this["veggies"].length;
-	  var meatCost = 2 * this["meat"].length;
-	  return pizzaCost + cheeseCost + meatCost + veggiesCost;
-	}
+	var pizzaSizes = {"sm" : 15, "md" : 17.50, "lg" : 19.00, "xl" : 22.5};
+	var pizzaCost = pizzaSizes [this["size"]];
+	var cheeseCost = 1 * this["cheese"].length;
+	var veggiesCost = 1.50 * this["veggies"].length;
+	var meatCost = 2 * this["meat"].length;
+	return pizzaCost + cheeseCost + meatCost + veggiesCost;
+}
